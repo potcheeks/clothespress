26184899a09eab010b505c79ee41a53e4701cde4
Charmaine Chan <chachan@Charmaines-MacBook-Pro.local>
2021-08-19 12:11:55 +0800

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundImage: (theme) => ({
-        'fa-img': "url('https://images.unsplash.com/photo-1551381912-4e2e29c7fd17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3450&q=80')"
-      })
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms')
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
   ],
+  theme: {},
+  variants: {},
+  plugins: [],
 }
